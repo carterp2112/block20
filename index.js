b1268a3833f6c62f0d4c83b0605f50563684e945
carterp2112 <st0pandsm3lltheroses@gmail.com>
2023-10-27 10:30:27 -0500

    test commit

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // TODO: this file! :)
+let numbers = [];
+let evens = []; let odds = [];
+
+function addToBank() {
+    let input = document.forms["add-number"]["number"].value;
+    if (input === "") {
+        alert("Please add a number");
+        return false;
+    } else {
+        numbers.push(parseInt(input));
+    }
+
+    document.getElementById("bank").innerHTML = numbers;
+    document.forms["add-number"]["number"].value = "";
+}
+
+function sortBankOne() {
+    const firstVal = numbers.shift();
+    if(firstVal % 2 === 0) {
+        evens.push(firstVal);
+    } else {
+        odds.push(firstVal);
+    }
+
+    document.getElementById("bank").innerHTML = numbers;
+    document.getElementById("odd-bank").innerHTML = odds;
+    document.getElementById("even-bank").innerHTML = evens;
+}
+
+function sortBankAll() {
+    evens = []; odds = [];
+    for (let i of numbers) {
+        if (i % 2 === 0) {
+            evens.push(i);
+        } else {
+            odds.push(i);
+        }
+    }
+
+    numbers = []
+    document.getElementById("bank").innerHTML = numbers;
+    document.getElementById("odd-bank").innerHTML = odds;
+    document.getElementById("even-bank").innerHTML = evens;
+}
